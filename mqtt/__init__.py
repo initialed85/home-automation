93407d9047042653337a76fc15f2233e9ff1a245
Edward Beech <initialed85@gmail.com>
2018-05-13 14:28:50 +0800

    Adding the Open Weather Map stuff

---
+++ mqtt/__init__.py
@@ -0,0 +1,5 @@
+from .client import NotConnectedError, UnknownTopicError, create_mqtt_client
+
+_ = create_mqtt_client
+_ = NotConnectedError
+_ = UnknownTopicError
---
+++ open_weather_map/__init__.py
@@ -0,0 +1,4 @@
+from .client import create_open_weather_map_client, RequestsPerMinuteExceededError
+
+_ = create_open_weather_map_client
+_ = RequestsPerMinuteExceededError
